017fd45e10e816594a04a0ec1940b0a8fde4c0f7
adrian abazi <adrian.abazi@wwu.de>
2020-11-15 19:46:26 +0100

    Server ExportManager

--- MiReQu-Server/app.py
+++ MiReQu-Server/app.py
@@ -16,6 +16,18 @@ from TimeTagger import Coincidences, Counter, Correlation, createTimeTagger, fre
 
 " Start Server from Terminal with >>> flask run --host=192.168.2.100"
 
+"""
+Here is an Example for the structure of the JsonFiles for the export:
+tstJsonExport = '{"Exercise":"Hauptversuch", "Subexercise":"Aufgabe1"' \
+                ',"FileName": "TstName"' \
+                ',"xAxis": [0,1,2,3,4]' \
+                ',"yAxis1": [0.0,10.0,20.0,30.0,40.0]' \
+                ',"yAxis2": [0.0,12.0,22.0,32.0,42.0]' \
+                ',"yAxis3": [0.0,13.0,23.0,33.0,43.0]' \
+                ' }'
+"""
+
+
 app = Flask(__name__)
 CORS(app)
 
