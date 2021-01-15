8e8b94c323bd2e073e8356ef63e37a0af6824141
adrian abazi <adrian.abazi@wwu.de>
2021-01-15 14:31:40 +0100

    Multiprocessing Part1

--- MiReQu-Server/app_20210115.py
+++ MiReQu-Server/app_20210115.py
@@ -9,10 +9,11 @@ from flask_cors import CORS
 import sys, os
 import PySide2
 import numpy as np
+import time
 
 # all required TimeTagger dependencies
 from TimeTagger import Coincidences, Counter, Correlation, createTimeTagger, freeTimeTagger
-
+from multiprocessing import Process, Value
 
 " Start Server from Terminal with >>> flask run --host=192.168.2.100"
 
@@ -31,37 +32,25 @@ tstJsonExport = '{"Exercise":"Hauptversuch", "Subexercise":"Aufgabe1"' \
 app = Flask(__name__)
 CORS(app)
 
-dirname = os.path.dirname(PySide2.__file__)
-plugin_path = os.path.join(dirname, 'plugins', 'platforms')
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+c1 ,c2, cc12 = [],[],[]
+p00,p01,p02 = 0,0,0
+s01,s02 = 0,0
 
-
-tagger = createTimeTagger()
-
-ccStream = CoincidencesNoUI(tagger)
-rEncoder = rotEncoderInterface("COM4")
-
-
-group = "BA-A-01" # GruppenNamen im Praktikum
-type = "Web3D"  # or "MR"
-exportpath = "c:/ExportMiReQu/"
-
-exportManager = ExportManager(group,type,exportpath)
 
 @app.route('/datagettimetagger', methods=['GET', 'POST'])
 def dataGetTimetagger():
     # POST request
     if request.method == 'POST':
+        return 'OK', 200
         print('Incoming..')
         print(request.get_json())  # parse as JSON
-        return 'OK', 200
 
     # GET request
     else:
         message = {
-            'C1': np.flip(ccStream.counter.getData()[0]* ccStream.getCouterNormalizationFactor()).tolist(),
-            'C2': np.flip(ccStream.counter.getData()[1]* ccStream.getCouterNormalizationFactor()).tolist(),
-            'CC12': np.flip(ccStream.counter.getData()[2] * ccStream.getCouterNormalizationFactor()).tolist(),
+            'C1': c1,#np.flip(ccStream.counter.getData()[0]* ccStream.getCouterNormalizationFactor()).tolist(),
+            'C2': c2,#np.flip(ccStream.counter.getData()[1]* ccStream.getCouterNormalizationFactor()).tolist(),
+            'CC12': cc12,#np.flip(ccStream.counter.getData()[2] * ccStream.getCouterNormalizationFactor()).tolist(),
             #'CR12': ccStream.correlation.getData().tolist()
         }
         return jsonify(message)  # serialize and use JSON headers
@@ -78,9 +67,9 @@ def dataGetRot():
     # GET request
     else:
         message = {
-            'p00': rEncoder.getPos00(),
-            'p01': rEncoder.getPos01(),
-            'p02': rEncoder.getPos02(),
+            'p00': p00,#rEncoder.getPos00(),
+            'p01': p01,#rEncoder.getPos01(),
+            'p02': p01,#rEncoder.getPos02(),
         }
         return jsonify(message)  # serialize and use JSON headers
 
@@ -95,8 +84,8 @@ def dataGetSens():
     # GET request
     else:
         message = {
-            's01': rEncoder.getSens01(),
-            's02': rEncoder.getSens02(),
+            's01': s01,#rEncoder.getSens01(),
+            's02': s02,#rEncoder.getSens02(),
         }
         return jsonify(message)  # serialize and use JSON headers
 
@@ -127,3 +116,34 @@ def dataPostExport():
     else:
         message = "this is used to recieve the export"
         return jsonify(message)  # serialize and use JSON headers
+
+
+def update_loop(loop_on):
+    dirname = os.path.dirname(PySide2.__file__)
+    plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
+    rEncoder = rotEncoderInterface("COM5")
+    tagger = createTimeTagger()
+
+    ccStream = CoincidencesNoUI(tagger)
+
+    group = "BA-A-01"  # GruppenNamen im Praktikum
+    type = "Web3D"  # or "MR"
+    exportpath = "c:/ExportMiReQu/"
+
+    exportManager = ExportManager(group, type, exportpath)
+
+    while True:
+      if loop_on.value == True:
+          p00, p01, p02,s01, s02 = rEncoder.getPos00(),rEncoder.getPos01(),rEncoder.getPos02(), rEncoder.getSens01(),rEncoder.getSens02()
+          print(p00, p01, p02,s01, s02)
+      time.sleep(0.05)
+
+
+if __name__ == "__main__":
+   update_on = Value('b', True)
+   p = Process(target=update_loop, args=(update_on,))
+   p.start()
+   app.run(use_reloader=False)
+   p.join()
--- MiReQu-Server/src/python/rotEncoderData.py
+++ MiReQu-Server/src/python/rotEncoderData.py
@@ -9,16 +9,50 @@ class rotEncoderInterface():
         self.ser.write("<check>".encode("utf8"))
         print("arduino at USB port " +comPort+" is " + self.posRead())
         time.sleep(.01)
+        self.faktorRot = 1./ 2.224603175
+        self.lastValuesRot = [0,0,0]
+        self.lastValuesSens = [0, 0, 0]
 
     def getPos00(self):
         self.ser.write("<get00>".encode("utf8"))
-        return int(self.posRead())
+        value = self.posRead()
+
+        try:
+            value = int(value)
+            value = value * self.faktorRot
+            value = value % 360
+            if value<0: value = value + 360.
+
+            self.lastValuesRot[0] = value
+        except: value = self.lastValuesRot[0]
+
+        return value
+
     def getPos01(self):
         self.ser.write("<get01>".encode("utf8"))
-        return int(self.posRead())
+        value = self.posRead()
+        try:
+            value = int(value)
+            value = value * self.faktorRot
+            self.lastValuesRot[1] = value
+            value = value % 360
+            if value < 0: value = value + 360.
+        except: value = self.lastValuesRot[1]
+
+        return value
+
     def getPos02(self):
         self.ser.write("<get02>".encode("utf8"))
-        return int(self.posRead())
+        value = self.posRead()
+        try:
+            value = int(value)
+            value = value * self.faktorRot
+            self.lastValuesRot[2] = value
+            value = value % 360
+            if value < 0: value = value + 360.
+        except: value = self.lastValuesRot[2]
+
+        return value
 
     def getSens01(self):
         self.ser.write("<agt01>".encode("utf8"))
@@ -38,11 +72,22 @@ class rotEncoderInterface():
 
 
 if __name__ == '__main__':
-    rEncoder = rotEncoderInterface("COM3")
-    for i in range(1000):
-        print("RotEncoder:")
-        print(rEncoder.getPos00(),rEncoder.getPos01(),rEncoder.getPos02())
-        print("Voltage:")
-        print(rEncoder.getSens01()/1023.*5.,rEncoder.getSens02()/1023.*5.)
-        time.sleep(0.1)
+    import numpy as np
+    rEncoder = rotEncoderInterface("COM5")
+
+    #measurements = 50
+    #vs = np.zeros(measurements)
+#
+    #for j in range(10000000):
+    #    for i in range(measurements):
+    #        vs[i] = rEncoder.getSens01() / 1023. * 5.
+    #        time.sleep(1/measurements)
+    #    print("Voltage [mV]:")
+    #    print(np.average(vs)*1000,np.std(vs)*1000)
+
+    for j in range(10000000):
+        print(rEncoder.getPos00(),rEncoder.getPos01(),rEncoder.getPos02(), rEncoder.getSens01(), rEncoder.getSens02())
+        time.sleep(0.005)
+
+
     rEncoder.close()
